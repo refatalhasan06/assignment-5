70e6fff38aa59c1b5cc117623ed9f872acca60ea
refatalhasan04 <refat.hasan.1314@gmail.com>
2024-09-24 19:02:04 +0600

    basic javascript done

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,98 @@
-document.getElementById("donate-now-btn")
+
+// document.getElementById('donate-now-btn')
+//     addEventListener("click", function(){     
+//         document.getElementById('donation-section').classList.remove("hidden");
+//         document.getElementById('history-section').classList.add("hidden");
+
+//     });
+
+// document.getElementById('history-btn')
+//     addEventListener("click", function(){  
+//         document.getElementById('history-section').classList.remove("hidden");
+//         document.getElementById('donation-section').classList.add("hidden");
+
+//     });
+
+document.getElementById("donatenow-noa")
     .addEventListener("click", function(event){
         event.preventDefault();
-        console.log("Donate now button clicked");
-        window.location.href = "index.html";
+        console.log("Donate for Nowakhali button clicked");
+
+        const noaDonate = document.getElementById('input-amount-noakhali').value;
+        const totalBalance = document.getElementById("totalbalance").innerHTML;
+        const currentBalance = parseFloat(totalBalance);
+        
+        if(noaDonate >= 0 && currentBalance > noaDonate){
+            const noaDonation = parseFloat(noaDonate);
+            const newBalance = currentBalance - noaDonate;
+
+            const totalDonation = document.getElementById("total-donation-for-noa").innerText;
+            const ttlDonation = parseFloat(totalDonation);
+            const totalDonated = ttlDonation + noaDonation;
+
+
+
+            document.getElementById("totalbalance").innerText =  newBalance;
+            document.getElementById("total-donation-for-noa").innerText =  totalDonated;
+
+        }
+        else{
+            alert("Unable to Donate");
+        }
     });
 
-document.getElementById("history-btn")
+document.getElementById("donatenow-feni")
     .addEventListener("click", function(event){
         event.preventDefault();
-        console.log("History button clicked");
-        window.location.href = "history.html";
+        console.log("Donate for Feni button clicked");
+
+        const feniDonate = document.getElementById('input-amount-feni').value;
+        const totalBalance = document.getElementById("totalbalance").innerHTML;
+        const currentBalance = parseFloat(totalBalance);
+        
+        if(feniDonate >= 0 && currentBalance > feniDonate){
+            const feniDonation = parseFloat(feniDonate);
+            const newBalance = currentBalance - feniDonate;
+
+            const totalDonation = document.getElementById("total-donation-for-feni").innerText;
+            const ttlDonation = parseFloat(totalDonation);
+            const totalDonated = ttlDonation + feniDonation;
+
+
+
+            document.getElementById("totalbalance").innerText =  newBalance;
+            document.getElementById("total-donation-for-feni").innerText =  totalDonated;
+
+        }
+        else{
+            alert("Unable to Donate");
+        }
+    });
+
+document.getElementById("donatenow-q")
+    .addEventListener("click", function(event){
+        event.preventDefault();
+        console.log("Donate for q button clicked");
+
+        const qDonate = document.getElementById('input-amount-q').value;
+        const totalBalance = document.getElementById("totalbalance").innerHTML;
+        const currentBalance = parseFloat(totalBalance);
+        
+        if(qDonate >= 0 && currentBalance > qDonate){
+            const qDonation = parseFloat(qDonate);
+            const newBalance = currentBalance - qDonate;
+
+            const totalDonation = document.getElementById("total-donation-for-q").innerText;
+            const ttlDonation = parseFloat(totalDonation);
+            const totalDonated = ttlDonation + qDonation;
+
+
+
+            document.getElementById("totalbalance").innerText =  newBalance;
+            document.getElementById("total-donation-for-q").innerText =  totalDonated;
+
+        }
+        else{
+            alert("Unable to Donate");
+        }
     });
